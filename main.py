a76f754cb856cba8edea8c2eea53e11c1bf66ccf
Elynor Kamil <92984338+Elynor-Kamil@users.noreply.github.com>
2024-07-02 21:06:56 +0100

    adding basic imports and initialising command

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+# discord specific imports
+from discord import Intents, Client, Message, app_commands
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# general imports
+from dotenv import load_dotenv
+import os
+
+from typing import Final
+
+# Load token from .env file
+load_dotenv()
+TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
+print(TOKEN)
+
+# Initialise discord bot settings
+intents: Intents = Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# main function
+def main() -> None:
+    bot.run(token=TOKEN)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# main
+if __name__ == "__main__":
+    main()
